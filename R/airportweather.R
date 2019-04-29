3b7e19a315d1ea53baaecb3e498e83e4030cc7c9
floatgreen <yikelikeli@gmail.com>
2019-04-28 20:16:37 -0500

    to remove "no visible binding for global variable "  and  "shiny note"

---
+++ R/airportweather.R
@@ -0,0 +1,18 @@
+#' \code{airportweather} package
+#'
+#' airportweather package
+#'
+#' @docType package
+#' @name airportweather
+#' @importFrom stats na.omit time
+#' @importFrom utils head tail
+#' @import shiny
+NULL
+
+utils::globalVariables(c("."))
+
+utils::globalVariables(c("fulltime", "code", "localtime", "time_UTC", "Weather", "Temperature", "hday"))
+
+utils::globalVariables(c("localtime", "Temperature", "hday"))
+
+utils::globalVariables(c("..level..", "element", "group", "lat", "latitude", "long",  "longitude", "station_id"))
